3473db58517b3f6fa0c38935898019bfb136f79b
iftesam15 <143783957+iftesam15@users.noreply.github.com>
2024-12-23 17:10:39 +0600

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -38,7 +38,7 @@ const Home = () => {
 
   return (
     <div className="p-4">
-      {/* <div className="flex justify-center items-center gap-x-4">
+      <div className="flex justify-center items-center gap-x-4">
         <button
           className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
           onClick={() => setShowType("table")}
@@ -70,8 +70,7 @@ const Home = () => {
       <UseMyContext></UseMyContext>
       <UseRef></UseRef>
 
-     
-      <TagManager></TagManager> */}
+      <TagManager></TagManager>
       <CounterProvider>
         <UseCountContext></UseCountContext>
       </CounterProvider>
